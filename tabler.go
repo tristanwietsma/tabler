6836e2d966e07aef087a698d37fce85f2e9c1e01
Tristan Wietsma <tristan.wietsma@gmail.com>
2015-01-04 22:40:38 -0600

    untested

--- tabler.go
+++ tabler.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"go/ast"
 	"go/parser"
@@ -9,25 +10,42 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
-	//"text/template"
+	"text/template"
 )
+
+var (
+	filters = template.FuncMap{
+		"plus1":  func(x int) int { return x + 1 },
+		"lower":  func(s string) string { return strings.ToLower(s) },
+		"caller": func(s string) string { return strings.ToLower(s)[0:1] },
+	}
+)
+
+func newTmpl(s string) *template.Template {
+	return template.Must(template.New("T").Funcs(filters).Parse(s))
+}
 
 // Column is a column in an SQL table.
 type Column struct {
 	Name       string
 	Type       string
 	IsPrimary  bool
+	IsForeign  bool
 	ForeignKey string
 }
 
 // Init sets the columns fields.
 func (c *Column) Init(name, tag string) error {
-	(*c).Name = strings.ToLower(name)
+	(*c).Name = name
 
-	if len((*c).Name) > 2 && (*c).Name[len((*c).Name)-2:] == "id" {
-		(*c).ForeignKey = fmt.Sprintf("references %s(id)", (*c).Name[:len((*c).Name)-2])
+	// auto-detect foreign key
+	if len(name) > 2 && name[len(name)-2:] == "ID" {
+		(*c).IsForeign = true
+		tbl := strings.ToLower((*c).Name[:len((*c).Name)-2])
+		(*c).ForeignKey = fmt.Sprintf("REFERENCES %s(id)", tbl)
 	}
 
+	// parse attributes
 	attributes := strings.Split(
 		strings.Trim(tag, "`"),
 		",",
@@ -43,6 +61,7 @@ func (c *Column) Init(name, tag string) error {
 			(*c).Type = pair[1]
 		case "primary":
 			(*c).IsPrimary = true
+
 		default:
 			return fmt.Errorf("Unknown attribute: '%s'", pair[0])
 		}
@@ -51,40 +70,58 @@ func (c *Column) Init(name, tag string) error {
 	return nil
 }
 
+func (c Column) String() string {
+	buf := bytes.Buffer{}
+	tmpl := newTmpl(`{{lower .Name}} {{.Type}}{{if .IsForeign}} {{.ForeignKey}}{{end}}`)
+	tmpl.Execute(&buf, c)
+	return buf.String()
+}
+
 // Table is an SQL table.
 type Table struct {
-	Name    string
-	Columns []Column
+	Name        string
+	Columns     []Column
+	PrimaryKeys []Column
 }
 
 // CreateTable returns a create table statement for the table.
 func (t Table) CreateTable() string {
-	return "CREATE TABLE" // to do
+	buf := bytes.Buffer{}
+	tmpl := newTmpl(`func ({{caller .Name}} {{.Name}}) CreateTable() string {
+    return ` + "`" + `CREATE TABLE {{lower .Name}} ({{$n := len .Columns}}{{range $i, $c := .Columns}}{{$c.String}}{{if lt (plus1 $i) $n}}, {{end}}{{end}}){{$p := len .PrimaryKeys}}{{if $p}} PRIMARY KEY ({{range $j, $k := .PrimaryKeys}}{{lower $k.Name}}{{if lt (plus1 $j) $p}}, {{end}}{{end}}){{end}};` + "`" + `
+}`)
+	tmpl.Execute(&buf, t)
+	return buf.String()
 }
 
 // DropTable returns a drop table statement for the table.
 func (t Table) DropTable() string {
-	return "DROP TABLE" // to do
+	buf := bytes.Buffer{}
+	tmpl := newTmpl(`func ({{caller .Name}} {{.Name}}) DropTable() string {
+    return ` + "`" + `DROP TABLE {{lower .Name}};` + "`" + `
+}`)
+	tmpl.Execute(&buf, t)
+	return buf.String()
 }
 
-// InsertRow returns an insert row statement for the table.
+// InsertRow returns a parameterized insertion statement for the table.
 func (t Table) InsertRow() string {
-	return "INSERT ROW" // to do
+	buf := bytes.Buffer{}
+	tmpl := newTmpl(`func ({{caller .Name}} {{.Name}}) InsertRow() string {
+    return ` + "`" + `INSERT INTO {{lower .Name}} ({{$n := len .Columns}}{{range $i, $c := .Columns}}{{$c.Name}}{{if lt (plus1 $i) $n}}, {{end}}{{end}}) VALUES ({{range $i, $c := .Columns}}?{{if lt (plus1 $i) $n}}, {{end}}{{end}});` + "`" + `
+}`)
+	tmpl.Execute(&buf, t)
+	return buf.String()
 }
 
-// DeleteRow returns a delete row statement for the table.
-func (t Table) DeleteRow() string {
-	return "DELETE ROW" // to do
-}
-
-// UpdateRow returns an update row statement for the table.
-func (t Table) UpdateRow() string {
-	return "UPDATE ROW" // to do
-}
-
-// SelectRow returns a select row statement for the table.
+// SelectRow returns a parameterized query for a single row.
 func (t Table) SelectRow() string {
-	return "SELECT ROW" // to do
+	buf := bytes.Buffer{}
+	tmpl := newTmpl(`func ({{caller .Name}} {{.Name}}) SelectRow() string {
+    return ` + "`" + `SELECT {{$n := len .Columns}}{{range $i, $c := .Columns}}{{lower $c.Name}}{{if lt (plus1 $i) $n}}, {{end}}{{end}} FROM {{lower .Name}} WHERE {{$p := len .PrimaryKeys}}{{range $j, $k := .PrimaryKeys}}{{lower $k.Name}}=?{{if lt (plus1 $j) $p}} AND {{end}}{{end}};` + "`" + `
+}`)
+	tmpl.Execute(&buf, t)
+	return buf.String()
 }
 
 // InputFile is a go file with tables.
@@ -153,7 +190,7 @@ func (i *InputFile) Init(path string) error {
 				if ts.Name == nil {
 					continue
 				}
-				table.Name = strings.ToLower(ts.Name.Name)
+				table.Name = ts.Name.Name
 				break
 			}
 		}
@@ -176,8 +213,12 @@ func (i *InputFile) Init(path string) error {
 				)
 			}
 			table.Columns = append(table.Columns, col)
+			if col.IsPrimary {
+				table.PrimaryKeys = append(table.PrimaryKeys, col)
+			}
 		}
-		if len(table.Columns) > 0 {
+
+		if len(table.Columns) > 0 && len(table.PrimaryKeys) > 0 {
 			(*i).Tables = append((*i).Tables, table)
 		}
 	}
@@ -186,7 +227,23 @@ func (i *InputFile) Init(path string) error {
 }
 
 func (i InputFile) Write() error {
-	fmt.Println(i)
+	buf := bytes.Buffer{}
+	tmpl := newTmpl(`// generated by tabler
+package {{.PackageName}}
+{{range $j, $t := .Tables}}
+// {{.Name}}
+
+{{$t.CreateTable}}
+
+{{$t.DropTable}}
+
+{{$t.InsertRow}}
+
+{{$t.SelectRow}}
+{{end}}`)
+	tmpl.Execute(&buf, i)
+
+	fmt.Println(buf.String())
 	return nil
 }
 
